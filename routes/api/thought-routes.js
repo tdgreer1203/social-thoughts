f755a36db6e762802b6a078e1c65af0c70f0f367
Theo <tdgreer1203@gmail.com>
2022-10-20 19:34:03 -0400

    Seeking Help With BCS

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 
-const { addThought, addReaction, deleteThought } = require('../../controllers/thought-controller');
+const { addThought, deleteThought } = require('../../controllers/thought-controller');
 
 router.route('/:userId').post(addThought);
 
 router.route('/:userId/:thoughtId').delete(deleteThought);
-
-router.route('/:thoughtId').post(addReaction);
 
 module.exports = router;
